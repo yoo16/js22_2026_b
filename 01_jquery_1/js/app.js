9a3e425e0c9836be16c5f39386bd9f49d7933925
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2026-04-22 10:29:48 +0900

    update

--- 01_jquery_1/js/app.js
+++ 01_jquery_1/js/app.js
@@ -1,7 +1,7 @@
 // バニラJSで、DOM読み込み後にアラート表示
-// document.addEventListener("DOMContentLoaded", function () {
-//     alert("バニラJS")
-// })
+document.addEventListener("DOMContentLoaded", function () {
+    alert("バニラJS")
+})
 
 // TODO: jQueryで、DOM読み込み後にアラート表示
 // jqdoc と入力して jqReadyShort を選択
@@ -11,3 +11,7 @@ $(function () {
 
 // TODO:DOM読み込み前の処理
 alert("はじまります！")
+
+// ブラウザのキャッシュ方法
+// Win: Ctrl + F5, Crl + Shift + R
+// Mac: Command + Shift + R
