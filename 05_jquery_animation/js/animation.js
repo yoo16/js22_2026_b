ebc31514b98705d586be54e566bfddc9207f88fa
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2026-05-20 10:45:44 +0900

    update

--- 05_jquery_animation/js/animation.js
+++ 05_jquery_animation/js/animation.js
@@ -61,10 +61,14 @@ $(function () {
             if (count > 0) {
                 // console.log(element);
                 // TODO: アニメーションで左に揺らす
+                element.animate({ left: -distance }, delay)
                 // TODO: アニメーションで右に揺らす
+                element.animate({ left: distance }, delay)
                 // TODO: コールバックで再帰的に shake() を呼び出す
+                shake(element, count - 1);
             } else {
                 // TODO: 最後に元の位置へ戻す
+                element.animate({ left: 0 }, delay);
             }
         }
         shake(character_4, times);
