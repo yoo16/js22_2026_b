9f9ec41f87b1299963c4b239b336af4f88f8d4b9
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2026-05-20 10:19:00 +0900

    update

--- 05_jquery_animation/js/animation.js
+++ 05_jquery_animation/js/animation.js
@@ -73,7 +73,17 @@ $(function () {
     $("#easingBtn").on("click", function () {
         initBox();
         // TODO: box1 を width=300px でアニメーション: easing=linear
+        box1.animate(
+            { width: "300px" },
+            1500,
+            "linear"
+        )
         // TODO: box2 を width=300px でアニメーション: easing=swing
+        box2.animate(
+            { width: "300px" },
+            1500,
+            "swing"
+        )
     })
 
     $("#animateBtn").on("click", function () {
