097062694bda92f3dea1cf1129bf63770f4b4201
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2026-06-03 10:36:46 +0900

    update

--- 09_api_1/json/js/app.js
+++ 09_api_1/json/js/app.js
@@ -13,6 +13,9 @@ document.addEventListener('DOMContentLoaded', function () {
     async function loadUser() {
         try {
             errorElement.textContent = '';
+            // TODO: API の URL を指定
+            const API_URL = './data/users.json';
+
             // TODO: JSONファイルをフェッチして、ユーザ情報を取得
             const response = {};
             // TODO: 非同期 で JSONデータをJavaScriptオブジェクトに変換: response.json() を使用
